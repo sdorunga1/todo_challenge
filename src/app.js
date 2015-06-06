9ab8e78dea10bb0c1ba81320dce525cdc822cf4d
Stefan Dorunga <sdorunga@housetrip.com>
2015-06-06 17:36:46 +0100

    Attempts filtering

--- src/app.js
+++ src/app.js
@@ -3,7 +3,27 @@ var Todo = angular.module("TodoApp", []).
   var self = this;
   self.tasks = [];
   self.addTask = function(){
-    self.tasks.push({title: self.newTask});
+    self.tasks.push({title: self.newTask, done: false});
     self.newTask = undefined;
+  };
+  self.removeTask = function(taskName){
+    self.tasks = self.tasks.filter(function(task){
+      return task.title !== taskName;
+    });
+  };
+  self.clearAllTasks = function() {
+    self.tasks = []
+  };
+
+  self.updateFilter = function (query) {
+    if(query === "all"){
+      query = ''
+    };
+    self.search = {done: query};
+    console.log(self.search)
+  };
+  self.isApplicable = function(task){
+    console.log(task);
+    return task.done;
   };
 }]);
--- test/todoControllerSpec.js
+++ test/todoControllerSpec.js
@@ -16,6 +16,27 @@ describe("Controller: TodoCtrl", function(){
   it("when adding a task", function(){
     ctrl.newTask = "New Task";
     ctrl.addTask();
-    expect(ctrl.tasks).toEqual([{title: "New Task"}]);
+    expect(ctrl.tasks).toEqual([{title: "New Task", done: false}]);
+  });
+
+  it("when deleting a task", function(){
+    ctrl.tasks = [{title: "Task 1"}, {title: "Task 2"}];
+    ctrl.removeTask("Task 1");
+    expect(ctrl.tasks).toEqual([{title: "Task 2"}]);
+  });
+
+  it("when clearing all tasks", function() {
+    ctrl.tasks = [{title: "Task 1"}, {title: "Task 2"}];
+    ctrl.clearAllTasks()
+    expect(ctrl.tasks).toBeEmpty
+  });
+
+  it("when filtering all sets the search to the query string", function() {
+    ctrl.tasks = [
+      {title: "Task 1", done: true},
+      {title: "Task 2", done: false}
+    ];
+    ctrl.filter('all');
+    expect(ctrl.search).toEqual({done: ''})
   });
 });
